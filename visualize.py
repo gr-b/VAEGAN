20f21e9157ea65b50438223e8ace2ea3507a2d46
gr-b <grbishop@wpi.edu>
2019-10-09 19:31:07 -0400

    Visualize.py works

--- visualize.py
+++ visualize.py
@@ -21,43 +21,43 @@ testLoader = torch.utils.data.DataLoader(
 ##############################
 
 
-if os.path.exists('./checkpoints/model.pt'):
-	model = torch.load('./checkpoints/model.pt')
-	print("Found model! Loading...")
-	
-	images, labels = iter(testLoader).next()
-	images = Variable(images).cuda()
-	
-	encoded_means, encoded_logvars = model.forward_encoder(images)
-	encoded_means, encoded_logvars = encoded_means.detach().cpu(), encoded_logvars.detach().cpu()
-
-	sizes = np.exp(0.5*encoded_logvars[:,0])*100*2
-
-	fig, ax = plt.subplots(1, 2)
-	ax[0].scatter(encoded_means[:,0], encoded_means[:,1],
-		c=labels, s=sizes, cmap='tab10')
-
-	def onclick(event):
-		global flag
-		ix, iy = event.xdata, event.ydata
-
-		try:	
-			latent_vec = torch.tensor([ix, iy])
-		except:
-			return	
-		latent_vec = Variable(latent_vec, requires_grad=False).cuda()
-		
-		decoded_img = model.forward_decoder(latent_vec)
-		decoded_img = decoded_img.detach().cpu().numpy().reshape(28, 28)
-		
-		ax[1].imshow(decoded_img, cmap='gray')
-		plt.draw()
-
-	cid = fig.canvas.mpl_connect('motion_notify_event', onclick)
-	plt.show()
-
-
-	exit()
+if os.path.exists('./checkpoints/enc.pt') and os.path.exists('./checkpoints/dec.pt') and os.path.exists('./checkpoints/disc.pt') :
+	print("Found models! Loading...")
 else:
-	print("No model found in checkpoints folder")
+	print("No models found in checkpoints folder. Exiting.")
+	exit()
 
+enc  = torch.load('./checkpoints/enc.pt')
+dec  = torch.load('./checkpoints/dec.pt')
+disc = torch.load('./checkpoints/disc.pt')
+
+
+images, labels = iter(testLoader).next()
+images = Variable(images).cuda()
+
+encoded_means, encoded_logvars = enc(images)
+encoded_means, encoded_logvars = encoded_means.detach().cpu(), encoded_logvars.detach().cpu()
+
+sizes = np.exp(0.5*encoded_logvars[:,0])*100*2
+
+fig, ax = plt.subplots(1, 2)
+ax[0].scatter(encoded_means[:,0], encoded_means[:,1],
+	c=labels, s=sizes, cmap='tab10')
+
+def onclick(event):
+	global flag
+	ix, iy = event.xdata, event.ydata
+	try:
+		latent_vec = torch.tensor([ix, iy])
+	except:
+		return
+	latent_vec = Variable(latent_vec, requires_grad=False).cuda()
+
+	decoded_img = dec(latent_vec)
+	decoded_img = decoded_img.detach().cpu().numpy().reshape(28, 28)
+
+	ax[1].imshow(decoded_img, cmap='gray')
+	plt.draw()
+
+cid = fig.canvas.mpl_connect('motion_notify_event', onclick)
+plt.show()
